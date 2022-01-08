ff0da06f1145e4b78c49c071a4675adaa66faf9b
Alex Resnick <adr8292@gmail.com>
2022-01-07 20:28:39 -0600

    trest

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,11 +1,8 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
-
 import edu.wpi.first.wpilibj.TimedRobot;
-
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -13,12 +10,10 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * project.
  */
 public class Robot extends TimedRobot {
-
   @Override
   public void robotInit() {
     System.out.println("Robot Init");
   }
-
   @Override
   public void robotPeriodic() {
     System.out.println("Robot Periodic");
