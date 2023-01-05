73c719627ef745eabe75ddd8515e83642eb0874b
Alex Resnick <adr8292@gmail.com>
2023-01-04 22:40:25 -0600

    fix axis button

--- src/main/java/frc/lib/AxisButton.java
+++ src/main/java/frc/lib/AxisButton.java
@@ -1,19 +1,14 @@
 package frc.lib;
 
-import static edu.wpi.first.wpilibj.util.ErrorMessages.requireNonNullParam;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController.Axis;
-import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 
 /**
- * A {@link Axis} treated as a {@link Button} that gets its state from a {@link GenericHID}.
+ * A {@link Axis} treated as a {@link Trigger} that gets its state from a {@link GenericHID}.
  */
-public class AxisButton extends Button {
-    private final GenericHID m_joystick;
-    private final int m_axisNumber;
-    private double m_sensitivity = 0.4;
-
+public class AxisButton extends Trigger {
     /**
      * Creates a joystick button for triggering commands.
      *
@@ -21,10 +16,7 @@ public class AxisButton extends Button {
      * @param axisNumber The axis number (see {@link GenericHID#getRawAxis(int) }
      */
     public AxisButton(GenericHID joystick, int axisNumber) {
-        requireNonNullParam(joystick, "joystick", "JoystickAxis");
-
-        m_joystick = joystick;
-        m_axisNumber = axisNumber;
+        super(() -> (Math.abs(joystick.getRawAxis(axisNumber)) > 0.4));
     }
 
     /**
@@ -35,19 +27,7 @@ public class AxisButton extends Button {
      * @param sensitivity The value the axis has to move to be considered as pressed
      */
     public AxisButton(GenericHID joystick, int axisNumber, double sensitivity) {
-        requireNonNullParam(joystick, "joystick", "JoystickAxis");
-        m_joystick = joystick;
-        m_axisNumber = axisNumber;
-        m_sensitivity = sensitivity;
+        super(() -> Math.abs(joystick.getRawAxis(axisNumber)) > sensitivity);
     }
 
-    /**
-     * Gets the value of the joystick button.
-     *
-     * @return The value of the joystick button
-     */
-    @Override
-    public boolean get() {
-        return Math.abs(m_joystick.getRawAxis(m_axisNumber)) > m_sensitivity;
-    }
 }
