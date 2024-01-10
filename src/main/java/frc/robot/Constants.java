fd8272bba8787319270f7d76153ce9dc23e0b9d5
Gael <gvaldez7206@stu.neisd.net>
2024-01-10 17:19:53 -0600

    fix changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,10 +3,18 @@ package frc.robot;
 /**
  * Constants file.
  */
-
 public final class Constants {
+    /**
+     * Stick Deadband
+     */
     public static final double stickDeadband = 0.1;
+    /**
+     * Driver ID
+     */
     public static final int driverID = 0;
+    /**
+     * Operator ID
+     */
     public static final int operatorID = 1;
 
     /**
@@ -21,13 +29,5 @@ public final class Constants {
     public static final class Pneumatics {
     }
 
-    /**
-     * Vision constants for limelight calculations.
-     */
-    public static final class VisionConstants {
-        public static final double deadPocket = 0.05;
-        public static final double limelightHeight = 0;
-        public static final double targetHeight = 0;
-        public static final double limelightAngle = 0;
-    }
+
 }
